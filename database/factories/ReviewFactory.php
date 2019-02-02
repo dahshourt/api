47c90e8e3701e1388ea4bef770aaf0f9f3263e35
unknown <walid.fci.94@gmail.com>
2019-02-02 13:44:46 +0200

    seeding part for product and review

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -4,6 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\model\review::class, function (Faker $faker) {
     return [
-        //
+        'product_id'=> function(){
+            return \App\model\product::all()->random();
+        },
+        'customer'=>$faker->name,
+        'review'=>$faker->paragraph,
+        'star'=>$faker->numberBetween(1,5)
+
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
         // $this->call(UsersTableSeeder::class);
+        factory(\App\model\product::class,50)->create();
+        factory(\App\model\review::class,300)->create();
+
     }
 }
